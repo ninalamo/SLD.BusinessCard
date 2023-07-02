49def095fd41a97b9c997008c88a3e9d8b477aa1
Nino Francisco Alamo <nin.alamo@outlook.com>
2023-07-02 21:28:32 +0800

    Done seedwork

---
+++ BusinessCard/BusinessCard.Domain/Seedwork/ICurrentUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCard.Domain.Seedwork
+{
+    public interface ICurrentUser
+    {
+        string Name { get; }
+        string Email { get; }
+        string IdentityId { get; }
+        string[] Roles { get; }
+
+
+        bool IsAdmin();
+    }
+}
