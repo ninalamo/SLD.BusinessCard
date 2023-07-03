51b8f9d9412192d170f573f90b5aca6489b63f02
Nino Francisco Alamo <nin.alamo@outlook.com>
2023-07-03 08:53:32 +0800

    Changed Company Enumeration to an Enttity Changed Coampany to IAggregateRoot

---
+++ BusinessCard/BusinessCard.Domain.Tests/CompanyTests.cs
@@ -0,0 +1,28 @@
+﻿using BusinessCard.Domain.AggregatesModel.CompanyAggregate;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCard.Domain.Tests
+{
+    public class CompanyTests
+    {
+        [Fact]
+        public void shouldBeAbleToCreate()
+        {
+            Company company = new("ABS-CBN");
+            company.Name.ShouldBe("ABS-CBN");
+        }
+
+        [Fact]
+        public void shouldBeAbleToRename()
+        {
+            Company company = new("ABS-CBN");
+            company.Name = "GMA";
+            company.Name.ShouldBe("GMA");
+        }
+    }
+}
---
+++ BusinessCard/BusinessCard.Domain/AggregatesModel/CompanyAggregate/Company.cs
@@ -0,0 +1,20 @@
+﻿using BusinessCard.Domain.Seedwork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCard.Domain.AggregatesModel.CompanyAggregate
+{
+    public class Company : Entity, IAggregateRoot
+    {
+        public string Name { get; set; }
+
+        public Company(string name)
+        {
+            Name = name;   
+        }
+        public static Company Create(string name) => new(name);
+    }
+}
