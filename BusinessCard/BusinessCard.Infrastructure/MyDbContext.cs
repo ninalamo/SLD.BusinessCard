365ea3b76c206219f926575dff4cd92564e366bb
Nino Francisco Alamo <nin.alamo@outlook.com>
2023-07-03 12:36:23 +0800

    Created DbContext

---
+++ BusinessCard/BusinessCard.Infrastructure/MyDbContext.cs
@@ -0,0 +1,40 @@
+﻿using BusinessCard.Domain.AggregatesModel.CompanyAggregate;
+using BusinessCard.Domain.AggregatesModel.NFCAggregate;
+using BusinessCard.Domain.Seedwork;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace BusinessCard.Infrastructure
+{
+    public class MyDbContext : DbContext, IUnitOfWork
+    {
+        public const string DEFAULT_SCHEMA = "business_card";
+        private readonly ICurrentUser _currentUser; //use in audit trail
+        private readonly IMediator _mediator; //use in domain events
+
+        private IDbContextTransaction _currentTransaction; //use in rollback-enabled transactions
+
+        #region Constructor
+        public MyDbContext(DbContextOptions<MyDbContext> options, IMediator mediator, ICurrentUser currentUser = default) : base(options)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _currentUser = currentUser;
+        }
+        #endregion
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<NfcCard> NfcCards { get; set; }
+
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+        public async Task<int> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
