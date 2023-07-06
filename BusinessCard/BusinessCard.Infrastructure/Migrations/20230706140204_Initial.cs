479082da485cd2007af97012860c51c0416d8247
Ninism <nin.alamo@outlook.com>
2023-07-06 22:05:03 +0800

    Initial migration

---
+++ BusinessCard/BusinessCard.Infrastructure/Migrations/20230706140204_Initial.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessCard.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "kardibee");
+
+            migrationBuilder.CreateTable(
+                name: "card",
+                schema: "kardibee",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Key = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedOn = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ModifiedOn = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_card", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "client",
+                schema: "kardibee",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Subscription = table.Column<int>(type: "int", nullable: false),
+                    IsDiscreet = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedOn = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ModifiedOn = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_client", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "people",
+                schema: "kardibee",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsSubsriptionOverride = table.Column<bool>(type: "bit", nullable: false),
+                    OverrideSubscription = table.Column<int>(type: "int", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MiddleName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NameSuffix = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SocialMedia = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Occupation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CardId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ClientId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedOn = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ModifiedOn = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_people", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_people_card_CardId",
+                        column: x => x.CardId,
+                        principalSchema: "kardibee",
+                        principalTable: "card",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_people_client_ClientId",
+                        column: x => x.ClientId,
+                        principalSchema: "kardibee",
+                        principalTable: "client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_client_CompanyName",
+                schema: "kardibee",
+                table: "client",
+                column: "CompanyName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_people_CardId",
+                schema: "kardibee",
+                table: "people",
+                column: "CardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_people_ClientId",
+                schema: "kardibee",
+                table: "people",
+                column: "ClientId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "people",
+                schema: "kardibee");
+
+            migrationBuilder.DropTable(
+                name: "card",
+                schema: "kardibee");
+
+            migrationBuilder.DropTable(
+                name: "client",
+                schema: "kardibee");
+        }
+    }
+}
