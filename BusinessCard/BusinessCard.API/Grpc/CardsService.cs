4151675f75f01fa2c8ffa6b68e0a165eb2e4cf2d
Ninism <nin.alamo@outlook.com>
2023-08-25 22:22:19 +0800

    Download urls

--- BusinessCard/BusinessCard.API/Grpc/CardsService.cs
+++ BusinessCard/BusinessCard.API/Grpc/CardsService.cs
@@ -1,5 +1,9 @@
 ﻿using BusinessCard.API.Application.Commands.ExportCards;
+using BusinessCard.API.Application.Queries.GetMembers;
 using BusinessCard.API.Extensions;
+using ClientService;
+using FluentValidation;
+using FluentValidation.Results;
 using Grpc.Core;
 using KardService;
 using MediatR;
@@ -26,5 +30,23 @@ public class KardsService : KardGrpc.KardGrpcBase
         result.Urls.AddRange(response.Urls);
     
         return result;
+    }
+
+    public override async Task<DownloadUrlsGrpcCommandResult> DownloadUrlsGrpc(DownloadUrlsGrpcCommand request, ServerCallContext context)
+    {
+        if (!Guid.TryParse(request.ClientId, out var guid))
+            throw new ValidationException("Validation Error",
+                new[] { new ValidationFailure("Id", "Id is not a Guid.") });
+
+        var data = await _mediator.Send(new GetMembersQuery(int.MaxValue,1,request.ClientId.ToGuid()));
+
+        var response = new DownloadUrlsGrpcCommandResult();
+        response.Urls.AddRange(data.Members
+            .Where(p => !p.IsActive && p.CardKey == "")
+            .Select(p => p.Id.ToString())
+            .ToArray());
+
+        return response;
+
     }
 }
