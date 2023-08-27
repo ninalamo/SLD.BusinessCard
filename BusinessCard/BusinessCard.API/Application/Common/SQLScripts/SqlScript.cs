4084f8491bb04bd602400f6c08c5faaffa500a3b
Ninism <nin.alamo@outlook.com>
2023-08-27 17:10:29 +0800

    Fixed SQL Error

--- BusinessCard/BusinessCard.API/Application/Common/SQLScripts/SqlScript.cs
+++ BusinessCard/BusinessCard.API/Application/Common/SQLScripts/SqlScript.cs
@@ -17,7 +17,7 @@ internal static class SqlScript
 	  		,(SELECT COUNT(*) FROM kardb.kardibee.people WHERE ClientId = C.Id AND C.IsActive = 0)  [NonCardholders] 
   		FROM [kardb].[kardibee].[client] C 
   			LEFT JOIN kardb.kardibee.people P ON P.ClientId = C.Id 
-  			LEFT JOIN kardb.kardibee.membertier M ON M.Id = C.[SubscriptionId] ";
+  			LEFT JOIN kardb.kardibee.membertier M ON M.Id = P.[SubscriptionId] ";
     
     public const string SelectClientById =  @"SELECT TOP 1
 	  		C.[Id] [ClientId] 
@@ -34,7 +34,7 @@ internal static class SqlScript
 	  		,(SELECT COUNT(*) FROM kardb.kardibee.people WHERE ClientId = C.Id AND C.IsActive = 0)  [NonCardholders] 
   		FROM [kardb].[kardibee].[client] C 
   			LEFT JOIN kardb.kardibee.people P ON P.ClientId = C.Id 
-  			LEFT JOIN kardb.kardibee.membertier M ON M.Id = C.[SubscriptionId] 
+  			LEFT JOIN kardb.kardibee.membertier M ON M.Id = P.[SubscriptionId] 
   		WHERE C.[Id] = @Id ";
 
     public const string SelectMembers = @"SELECT
