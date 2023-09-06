92f4db988087e835640237e839e5f9f773a31175
Ninism <nin.alamo@outlook.com>
2023-09-06 09:31:23 +0800

    Update SqlScript.cs

--- BusinessCard/BusinessCard.Application/Application/Common/SQLScripts/SqlScript.cs
+++ BusinessCard/BusinessCard.Application/Application/Common/SQLScripts/SqlScript.cs
@@ -56,7 +56,7 @@ internal static class SqlScript
 	      ,P.[CreatedOn]
 	      ,P.[ModifiedBy]
 	      ,P.[ModifiedOn]
-	      ,P.[IsActive],
+	      ,P.[IsActive]
     	  ,P.[IdentityUserId]
 	FROM [kardb].[kardibee].[people] P
 	LEFT JOIN kardb.kardibee.card C ON C.Id = P.CardId
