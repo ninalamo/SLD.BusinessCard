d43b35eb0f28019e16f11cb67ab0821c6231a133
Ninism <nin.alamo@outlook.com>
2023-09-14 10:20:21 +0800

    Card domain and unit test

---
+++ BusinessCard/BusinessCard.Domain.Tests/Domain/CardTest.cs
@@ -0,0 +1,91 @@
+using BusinessCard.Domain.AggregatesModel.ClientAggregate;
+using FluentValidation;
+using Shouldly;
+
+public class CardTest
+{
+    [Fact]
+    public void CanCreateCard()
+    {
+        Card card = new();
+        
+        card.ShouldNotBeNull();
+        card.HasUid().ShouldBeFalse();
+        card.IsActive.ShouldBeFalse();
+        card.Uid.ShouldBeEmpty();
+        
+        card.ActivatedDate.ShouldBeNull();
+        card.RenewDate.ShouldBeNull();
+        card.ExpireDate.ShouldBeNull();
+
+    }
+    
+    [Theory]
+    [InlineData("xxx",13)]
+    [InlineData("adadfxxx",2)]
+    [InlineData("xxadewrewfx",113)]
+    [InlineData("xadsfgstgr34534fdsrxx",1213)]
+    [InlineData("xxadsfadsfdasfx",33)]
+    [InlineData("xadsfadsfxx",3)]
+    [InlineData("xasdfadsxx",1)]
+    public void CardCanBeActivated(string uid, int months)
+    {
+        Card card = new();
+        card.Activate(uid,months);
+        card.Uid.ShouldBe(uid);
+        card.HasUid().ShouldBeTrue();
+        card.IsActive.ShouldBeTrue();
+        
+        card.ActivatedDate.ShouldNotBeNull();
+        card.ActivatedDate.Value.Date.ShouldBe(DateTime.Today.Date);
+
+        card.ExpireDate.ShouldNotBeNull();
+        card.ExpireDate.Value.Date.ShouldBe(DateTime.Today.AddMonths(months).Date);
+
+        card.RenewDate.ShouldBeNull();
+    }
+
+    [Theory]
+    [InlineData("xxx",0)]
+    [InlineData("xxx",-100)]
+    public void CardShouldThrowErrorWhenActivatedIncorrectly(string uid, int months)
+    {
+        Card card = new();
+        Assert.Throws<ValidationException>(() => card.Activate(uid, months));
+        card.IsActive.ShouldBeFalse();
+        card.HasUid().ShouldBeFalse();
+    }
+    
+    [Fact]
+    public void CardCanBeDeactivated()
+    {
+        Card card = new();
+        card.Activate("xxx", 13);
+        card.Deactivate();
+
+        card.IsActive.ShouldBeFalse();
+        card.ExpireDate.Value.Date.ShouldBe(DateTime.Today.Date);
+        card.RenewDate.ShouldBeNull();
+
+    }
+    
+    [Fact]
+    public void CardCanRenew()
+    {
+        Card card = new();
+        card.Activate("xxx", 13);
+        card.Deactivate();
+
+        card.Renew(5, null);
+
+        card.ExpireDate.ShouldNotBeNull();
+        card.ExpireDate.Value.Date.ShouldBe(DateTime.Today.AddMonths(5).Date);
+        card.RenewDate.Value.Date.ShouldBe(DateTime.Today.Date);
+        
+        card.Renew(1,DateTime.Today.AddMonths(4));
+        card.ExpireDate.Value.Date.ShouldBe(DateTime.Today.AddMonths(5).Date);
+        card.RenewDate.Value.Date.ShouldBe(DateTime.Today.AddMonths(4));
+        card.IsActive.ShouldBeTrue();
+
+    }
+}
--- BusinessCard/BusinessCard.Domain/AggregatesModel/ClientAggregate/Card.cs
+++ BusinessCard/BusinessCard.Domain/AggregatesModel/ClientAggregate/Card.cs
@@ -5,27 +5,58 @@ namespace BusinessCard.Domain.AggregatesModel.ClientAggregate
 {
     public sealed class Card : Entity
     {
-        public string Key { get; private set; }
-        public DateTimeOffset StartDate { get; private set; }
-        public DateTimeOffset ValidUntilDate { get; private set; }
+        public string Uid { get; private set; }
+        public DateTimeOffset? ActivatedDate { get; private set; }
+        public DateTimeOffset? ExpireDate { get; private set; }
         public DateTimeOffset? RenewDate { get; private set; }
         
 
         public Card()
         {
-            Key = string.Empty;
+            Uid = string.Empty;
+            IsActive = false;
         }
 
-        public void SetKey(string key)
+        private void SaveUid(string key)
         {
-            if(Key != string.Empty)
+            if(Uid != string.Empty)
             {
                 throw  new ValidationException("Business validation error. NFC Key is immutable");
             }
-            Key = key;
+            Uid = key;
         }
 
-        public bool HasKey() => !string.IsNullOrEmpty(Key);
+        public void Activate(string uid, int monthsBeforeExpire)
+        {
+            if (ActivatedDate.HasValue) throw new ValidationException("Already activated.");
+            if (monthsBeforeExpire <= 0) throw new ValidationException("Should expire at least after 1 month.");
+
+            SaveUid(uid);
+            
+            ActivatedDate = DateTimeOffset.Now;
+            ExpireDate = ActivatedDate.Value.AddMonths(monthsBeforeExpire);
+            IsActive = true;
+        }
+
+        public void Deactivate()
+        {
+            IsActive = false;
+            ExpireDate = DateTimeOffset.Now;
+        }
+        
+        public void Renew(int monthsBeforeExpire, DateTimeOffset? renewDateOptional)
+        {
+            if (!ActivatedDate.HasValue) throw new ValidationException("Cannot renew. Card inactive.");
+            if (monthsBeforeExpire <= 0) throw new ValidationException("Should expire at least after 1 month.");
+            if (renewDateOptional.HasValue && renewDateOptional.Value.Date < DateTime.Today.Date)
+                throw new ValidationException("Invalid renew date");
+
+            RenewDate = renewDateOptional ?? DateTimeOffset.Now;
+            ExpireDate = RenewDate.Value.AddMonths(monthsBeforeExpire);
+            IsActive = true;
+        }
+
+        public bool HasUid() => !string.IsNullOrEmpty(Uid);
 
     }
 }
